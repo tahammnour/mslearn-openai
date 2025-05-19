6e6524b79680057c0d0aa3be1cd03b0e0c7bc9d6
Taha Nour El-Dein <81038277+tahammnour@users.noreply.github.com>
2025-05-19 13:44:29 +0000

    edit owndata file

--- Labfiles/02-use-own-data/Python/ownData.py
+++ Labfiles/02-use-own-data/Python/ownData.py
@@ -12,10 +12,35 @@ deployment = os.environ.get("AZURE_OAI_DEPLOYMENT")
 client = openai.AzureOpenAI(
     azure_endpoint=endpoint,
     api_key=api_key,
-    api_version="2024-02-01",
+    api_version="2024-11-20",
 )
 
 # Configure your data source
-
+text = input('\nEnter a question:\n')
+    
+completion = client.chat.completions.create(
+     model=deployment,
+     messages=[
+         {
+             "role": "user",
+             "content": text,
+         },
+     ],
+     extra_body={
+         "data_sources":[
+             {
+                 "type": "azure_search",
+                 "parameters": {
+                     "endpoint": os.environ["AZURE_SEARCH_ENDPOINT"],
+                     "index_name": os.environ["AZURE_SEARCH_INDEX"],
+                     "authentication": {
+                         "type": "api_key",
+                         "key": os.environ["AZURE_SEARCH_KEY"],
+                     }
+                 }
+             }
+         ],
+     }
+ )
 
 print(completion.model_dump_json(indent=2))
